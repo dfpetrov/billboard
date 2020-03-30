24b91a43710fc988f39aa4081c15fa3559dd8636
dfgaraev <56137646+dfpetrov@users.noreply.github.com>
2020-03-30 17:10:03 +0300

    Init

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, Response
 from database.db import initialize_db
 from database.models import Ad
 import json
+import redis
 
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/ads'
 }
+
+client = redis.Redis(host='localhost', port=6379)
 
 initialize_db(app)
 
@@ -21,26 +24,37 @@ def add_ad():
     body = request.get_json()
     ad = Ad(**body).save()
     id = ad.id
-    return Response(ad.to_json(), mimetype="application/json", status=200)
+    ad_json = ad.to_json()
+    client.set(str(id), str(ad_json))
+    return Response(ad_json, mimetype="application/json", status=200)
 
 @app.route('/ad/<id>')
 def get_ad(id):
-    ad = Ad.objects.get(id=id).to_json()
-    return Response(ad, mimetype="application/json", status=200)
+    cache_dict = client.get(str(id))
+    result = {'откуда достали': 'из базы данных'}
+    if cache_dict:
+        result['откуда достали'] = 'из кэша'
+        result.update(json.loads(cache_dict.decode("utf-8")))
+    else:
+        result.update(json.loads(Ad.objects.get(id=id).to_json()))
+    return Response(json.dumps(result), mimetype="application/json", status=200)
 
 @app.route('/addtag/<id>', methods=['POST'])
 def addtag(id):
     ad = Ad.objects.get(id=id)
     ad.tags = json.loads(ad.to_json())['tags'] + request.get_json()['tags']
     ad.save()
-    return Response(ad.to_json(), mimetype="application/json", status=200)
+    ad_json = ad.to_json()
+    client.set(str(id), str(ad_json))
+    return Response(ad_json, mimetype="application/json", status=200)
 
 @app.route('/addcomm/<id>', methods=['POST'])
 def addcomm(id):
     ad = Ad.objects.get(id=id)
     ad.comments = json.loads(ad.to_json())['comments'] + request.get_json()['comments']
-    ad.save()
-    return Response(ad.to_json(), mimetype="application/json", status=200)
+    ad_json = ad.to_json()
+    client.set(str(id), str(ad_json))
+    return Response(ad_json, mimetype="application/json", status=200)
 
 @app.route('/stat/<id>')
 def stat(id):
@@ -50,6 +64,11 @@ def stat(id):
         'comment count': len(ad['comments'])
     }
     return Response(json.dumps(stat), mimetype="application/json", status=200)
+
+@app.route('/clear')
+def clear():
+    client.flushdb()
+    return Response('Кэш очищен', status=200)
 
 if __name__ == "__main__":
     app.run()
